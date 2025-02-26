f3753c35012c36bcb1dffaeb7602c39380ac4551
parmarjayit <jayparmarit28@gmail.com>
2025-02-26 12:42:45 +0530

    SWITCH GRADE COMPLET

--- index.cpp
+++ index.cpp
@@ -1,40 +1,67 @@
 #include<iostream>
+
 using namespace std;
 
 int main()
 {
-	int a = 10;
-	int b = 8;
-	int c = 5;
+	int marks;
+	 cout << "enter marks" << endl;
+	 cout <<"----------------" << endl;
 	
-	if(a > b)
+	cin >> marks ;
+	
+	cout <<"----------------" << endl;
+	char grade ;
+	
+	if(marks > 90)
 	{
-		if(a > c)
-		{
-			cout << " a is greatter" << endl;
-		}
-		else
-		{
-			cout << " c is greatter" << endl;
-		}
+		cout << "your grade is A++" << endl;
+		grade= 'A';
+	}
+	else if (marks > 80)
+	{
+		cout << "your grade is A" << endl;
+		grade= 'B';
 	}
 	
-	
-	
-	
-	
-	else
+	else if (marks > 70)
 	{
-		if(b > c)
-		{
-			cout << " b is greatter" << endl;
-		}
-		else
-		{
-			cout << " c is greatter" << endl;
-		}
+		cout << "your grade is B++" << endl;
+		grade= 'C';
 	}
 	
+	else if (marks > 60)
+	{
+		cout << "your grade is B" << endl;
+		grade= 'D';
+	}
+	
+	else if (marks > 50)
+	{
+		cout << "your grade is C" << endl;
+		grade= 'E';
+	}
+	else 
+	{
+		cout << " check your input" << endl;
+	}
+	
+	cout <<"----------------" << endl;
+	
+	switch (grade)
+	{
+		case 'A' : cout  <<" excelent" ;
+			break;
+		case 'B' : cout  <<"very job" ;
+			break;
+		case 'C' : cout  <<"good job" ;
+			break;
+		case 'D' : cout  <<"good" ;
+			break;
+		case 'E' : cout  <<"super" ;
+			break;
+		default : cout << "better luck...next time...!";	
+	}
 	
 	
 	
